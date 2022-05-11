280576d2581798d2a3cf633537af04d16512efab
Young woon Na <younni08@gmail.com>
2022-05-11 17:41:45 +0900

    noti

--- client/src/components/notification/notification.tsx
+++ client/src/components/notification/notification.tsx
@@ -3,7 +3,18 @@ import React from "react";
 const Notification = () => {
     return (
         <div className="noti">
+            <div>
+                <div className="noti_lv1">
+                    <img src="./noti.jpg" alt="books" />
+                    <div>
+                        <span>Notification</span>
+                        <span>Improvement from the TranslateLab</span>
+                    </div>
+                </div>
+                <div className="noti_main">
 
+                </div>
+            </div>
         </div>
     )
 }
